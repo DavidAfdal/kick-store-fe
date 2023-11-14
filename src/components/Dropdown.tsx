9bf41276ee98ff03cee283218fd2f9eadb19090d
David Afdal Kaizar Mutahadi <davidafdal7@gmail.com>
2023-11-14 19:36:10 +0700

    add dropdown icon  navbar

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,60 @@
-const Dropdown = () => {
-  return <div></div>;
+import React, { useState, useRef, useEffect } from 'react';
+import { BsFillPersonFill } from 'react-icons/bs';
+
+type DropdownProps = { children: React.ReactNode };
+
+const Dropdown: React.FC<DropdownProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    // Delay closing the dropdown to allow moving the mouse to the dropdown
+    setTimeout(() => {
+      if (dropdownRef.current) {
+        if (!dropdownRef.current.contains(document.activeElement)) {
+          closeDropdown();
+        }
+      }
+    }, 200); // Adjust the delay as needed
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className='relative w-[24px] h-[24px]' ref={dropdownRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <button type='button' className='bg-white w-full h-full' id='options-menu' aria-haspopup='true' aria-expanded={isOpen}>
+        <BsFillPersonFill className='text-2xl font-medium text-gray-700 focus:text-[#4A69E2] hover:text-[#4A69E2] ' />
+      </button>
+
+      {isOpen && (
+        <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none' role='menu' aria-orientation='vertical' aria-labelledby='options-menu'>
+          <div className='py-1' role='none'>
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import Button from './Button';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext, AuthContextType } from '../context/auth-context';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { BsFillPersonFill } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import Dropdown from './Dropdown';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -42,16 +42,19 @@ const Navbar = () => {
             {totaltemInCart() <= 0 ? null : <div className='absolute right-[-10px] top-[-10px] w-[20px] h-[20px] bg-[#4A69E2] text-white rounded-full flex items-center justify-center text-sm transition-all '>{totaltemInCart()}</div>}
           </div>
 
-          <BsFillPersonFill
-            className='text-2xl cursor-pointer'
-            id='menu-button'
-            aria-expanded='true'
-            aria-haspopup='true'
-            onClick={() => {
-              logout();
-              navigate('/');
-            }}
-          />
+          <Dropdown>
+            <p className='cursor-pointer text-base text-gray-700 px-4 py-2 hover:bg-gray-100'>Profile</p>
+            <p className='cursor-pointer text-base text-gray-700 px-4 py-2 hover:bg-gray-100'>Like</p>
+            <p
+              onClick={() => {
+                logout();
+                navigate('/');
+              }}
+              className='cursor-pointer text-base text-gray-700 hover:bg-gray-100 px-4 py-2'
+            >
+              Logout
+            </p>
+          </Dropdown>
         </div>
       ) : (
         <div className='flex gap-4 items-center'>
